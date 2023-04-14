fa45a43bd383bf37e1e15ac714d7fcc1e2ffbcfd
gangeshberi <gangeshberi@gmail.com>
2023-04-14 17:39:11 -0400

    added ESLint

--- api/controllers/drug_controller.js
+++ api/controllers/drug_controller.js
@@ -1,22 +1,22 @@
-const Drug = require('../models/drug_model')
+const Drug = require('../models/drug_model');
 
 const getDrugs = function(req, res) {
-    return Drug
-            .findAll()
-            .then(drugs => res.send(drugs))
-            .catch(err => res.send(err)); // important to catch the error
-}
+	return Drug
+		.findAll()
+		.then(drugs => res.send(drugs))
+		.catch(err => res.send(err)); // important to catch the error
+};
 
 const getDrug = function(req, res) {
-    const drugId = Number(req.params.drugId);
+	const drugId = Number(req.params.drugId);
     
-    return Drug
-            .findOne(drugId)
-            .then(drug => res.send(drug))
-            .catch(err => res.send(err)); // important to catch the error
-}
+	return Drug
+		.findOne(drugId)
+		.then(drug => res.send(drug))
+		.catch(err => res.send(err)); // important to catch the error
+};
 
 module.exports = {
-    getDrugs,
-    getDrug
-}
+	getDrugs,
+	getDrug
+};
