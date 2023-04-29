f9b55f20b2aba3a9bcb582f6cee9affaeda32db1
gangesh <gangeshberi@gmail.com>
2023-04-29 00:20:14 -0400

    added new seeding file and corresponding data for it

---
+++ db/seeds/01_drug.js
@@ -0,0 +1,38 @@
+const csv = require('csvtojson');
+const path = require('node:path');
+
+const drugFile = path.join(__dirname, '../data/drugs.csv');
+
+const parserParams = {
+  delimiter: ',', // optional
+  quote: '"', // optional
+  ignoreEmpty: true,
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function drugTable(knex) {
+  // Deletes ALL existing entries for drug table
+  try {
+    await knex('drug')
+      .del()
+      .then(() => console.log('<--- Successfully deleted drug table --->'));
+  } catch (error) {
+    console.error(`An error occurred while deleting drug table data, ${error}`);
+  }
+
+  // add new enteries to the drug table
+  try {
+    // drug JSON data array
+    const drugArray = await csv(parserParams).fromFile(drugFile);
+
+    // insert data into drug table
+    await knex('drug')
+      .insert(drugArray)
+      .then(() => console.log('<--- Successfully inserted data into drug table --->'));
+  } catch (error) {
+    console.error(`An error occurred while inserting data to drug table, ${error}`);
+  }
+};
