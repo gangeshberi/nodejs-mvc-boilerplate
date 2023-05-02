37257da54800004e3e97488936a79938194ec6f7
gangesh <gangeshberi@gmail.com>
2023-05-01 20:11:18 -0400

    used async await instead of promises in drug controller

--- api/controllers/drug_controller.js
+++ api/controllers/drug_controller.js
@@ -1,46 +1,54 @@
 const Drug = require('../models/drug_model');
 const logger = require('../../config/winston');
 
-const SERVER_ERROR = 'Internal server error';
-
 /**
+ * Get all drugs.
  *
  * @param {Object} req - request object
  * @param {Object} res - response object
  * @returns {Array|Object} - drug array or error response object
  */
-function getDrugs(req, res) {
-  return Drug
-    .findAll()
-    .then((drugs) => res.status(200).json(drugs))
-    .catch((err) => {
-      logger.error(`An Error occurred in getDrugs controller, ${err}`);
-      res.status(500).send({
-        message: SERVER_ERROR,
-        error: err,
-      });
+async function getDrugs(req, res) {
+  try {
+    const drugs = await Drug.findAll();
+    res.status(200).json(drugs);
+    logger.info('Retrieved data for all drugs using getDrugs controller function');
+  } catch (error) {
+    logger.error(`An error occurred in getDrugs controller: ${error.message}`);
+    res.status(500).send({
+      message: 'Failed to retrieve drugs',
+      error: error.message,
     });
+  }
 }
 
 /**
+ * Get a single drug by ID.
  *
  * @param {Object} req - request object
  * @param {Object} res - response object
  * @returns {Array|Object} - single drug array or error response object
  */
-function getDrug(req, res) {
-  const drugId = Number(req.params.drugId);
-
-  return Drug
-    .findOne(drugId)
-    .then((drug) => res.status(200).json(drug))
-    .catch((err) => {
-      logger.error(`An Error occurred in getDrug controller, ${err}`);
-      res.status(500).send({
-        message: SERVER_ERROR,
-        error: err,
-      });
+async function getDrug(req, res) {
+  try {
+    const drugId = Number(req.params.drugId);
+    if (Number.isNaN(drugId)) {
+      logger.error('Invalid drug ID');
+      throw new Error('Invalid drug ID');
+    }
+    const drug = await Drug.findOne(drugId);
+    if (!drug) {
+      throw new Error(`Drug with ID ${drugId} not found`);
+    }
+    res.status(200).json(drug);
+    logger.info('Retrieved data for a single drug using getDrug controller function');
+  } catch (error) {
+    logger.error(`An error occurred in getDrug controller: ${error.message}`);
+    res.status(500).send({
+      message: 'Failed to retrieve drug',
+      error: error.message,
     });
+  }
 }
 
 module.exports = {
